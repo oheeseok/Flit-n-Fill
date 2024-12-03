f441378b150f326e63c79dee96ff50f784f5d81e
oheeseok <heeseok96@gmail.com>
2024-12-03 17:34:44 +0900

    added recipe entity

---
+++ backend/src/main/java/org/example/backend/domain/recipe/entity/Recipe.java
@@ -0,0 +1,28 @@
+package org.example.backend.domain.recipe.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "recipe")
+@Getter
+@Setter
+public class Recipe {
+  @Id
+  private String recipeId; // MongoDB의 기본 키
+  private long userId;
+  private String recipeTitle;
+  private String recipeIngredientDetails;
+  private List<Step> recipeSteps;
+  private boolean recipeIsVisibility;
+
+  @Getter
+  @Setter
+  public static class Step {
+    private String photo;
+    private String description;
+  }
+}
