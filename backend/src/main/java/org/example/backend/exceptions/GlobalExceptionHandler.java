e243d4605af34e6e6112021246fe932bf429c57e
oheeseok <heeseok96@gmail.com>
2024-12-16 00:40:51 +0900

    feat: exception 추가 (#14)

---
+++ backend/src/main/java/org/example/backend/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package org.example.backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleUnauthorizedAccess(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access");
+    }
+}
