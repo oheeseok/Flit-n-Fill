79d30540bda6666886a09592aa359d673e34421b
oheeseok <heeseok96@gmail.com>
2024-12-18 17:20:10 +0900

    feat: Post DO, DTO 수정 - postTitle 직접 수정되게 변경함 (#10)

--- backend/src/main/java/org/example/backend/api/post/model/dto/PostRegisterDto.java
+++ backend/src/main/java/org/example/backend/api/post/model/dto/PostRegisterDto.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostRegisterDto {
+    private String postTitle;
     private String postContent;
     private String meetingPlace;
     private LocalDateTime meetingTime;
