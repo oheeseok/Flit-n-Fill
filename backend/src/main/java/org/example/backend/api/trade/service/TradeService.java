2bb66c5f00f21034fd4d80e323345ae64c4889c3
byulhan <hanbyul933@gmail.com>
2024-12-24 11:53:53 +0900

    feat: trade directory 추가

---
+++ backend/src/main/java/org/example/backend/api/trade/service/TradeService.java
@@ -0,0 +1,9 @@
+package org.example.backend.api.trade.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class TradeService {
+}
