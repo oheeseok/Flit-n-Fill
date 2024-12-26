b5c3915c155947b2d99cafa7ab46be85188371d6
byulhan <hanbyul933@gmail.com>
2024-12-26 15:53:46 +0900

    fix: repository 설정 수정

--- backend/src/main/java/org/example/backend/api/trade/repository/KindnessRepository.java
+++ backend/src/main/java/org/example/backend/api/trade/repository/KindnessRepository.java
@@ -1,10 +1,7 @@
 package org.example.backend.api.trade.repository;
 
 import org.example.backend.api.trade.model.entity.Kindness;
-import org.example.backend.api.trade.model.entity.TradeRoom;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface KindnessRepository extends MongoRepository<Kindness, Long> {
+public interface KindnessRepository extends JpaRepository<Kindness, Long> {
 }
