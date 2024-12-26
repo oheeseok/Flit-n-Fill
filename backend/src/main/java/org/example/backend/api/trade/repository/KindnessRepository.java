f591c961228dbe5d2a5571d5ca037e5b5835d36b
byulhan <hanbyul933@gmail.com>
2024-12-26 17:19:20 +0900

    fix: kindness 조회 조건을 거래방과 유저아이디 동시에 만족하도록 수정(#75)

--- backend/src/main/java/org/example/backend/api/trade/repository/KindnessRepository.java
+++ backend/src/main/java/org/example/backend/api/trade/repository/KindnessRepository.java
@@ -3,6 +3,9 @@ package org.example.backend.api.trade.repository;
 import org.example.backend.api.trade.model.entity.Kindness;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface KindnessRepository extends JpaRepository<Kindness, Long> {
-    Kindness findByTradeRoomId(String tradeRoomId);
+    Optional<Kindness> findByTradeRoomIdAndReviewer_UserId(String tradeRoomId, Long userId);
 }
