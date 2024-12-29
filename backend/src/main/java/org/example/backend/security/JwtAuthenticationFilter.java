a4aedf4760ee1a1c384b498ebf818994b599f88a
byulhan <hanbyul933@gmail.com>
2024-12-29 14:47:23 +0900

    fix: userEmail, token 유효성 검증 로직 위치 변경

--- backend/src/main/java/org/example/backend/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/org/example/backend/security/JwtAuthenticationFilter.java
@@ -72,17 +72,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         log.info("[JwtAuthenticationFilter] cookie accessToken : {}", token);
 
         if (token != null && userEmail != null) {
-<<<<<<< Updated upstream
-//            if (!userEmail.equals(jwtTokenProvider.getUserEmailFromToken(token))) {
-//                log.error("[JwtAuthenticationFilter] User email mismatch: token={}, request={}",
-//                        jwtTokenProvider.getUserEmailFromToken(token), userEmail);
-//                response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403 Forbidden
-//                response.getWriter().write("User email mismatch");
-//                return;
-//            }
-
-=======
->>>>>>> Stashed changes
             try {
                 token = tokenManagementService.isTokenExpired(userEmail, token, response);
                 if (token == null) {
