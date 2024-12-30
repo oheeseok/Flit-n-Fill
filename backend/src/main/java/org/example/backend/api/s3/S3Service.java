96c5a3d3339aa00a8c286cc86a1e7717966ea806
oheeseok <heeseok96@gmail.com>
2024-12-30 18:28:30 +0900

    feat: S3 파일 삭제 함수 추가 (#103)

--- backend/src/main/java/org/example/backend/api/s3/S3Service.java
+++ backend/src/main/java/org/example/backend/api/s3/S3Service.java
@@ -1,17 +1,22 @@
 package org.example.backend.api.s3;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.backend.exceptions.S3FileNotDeletedException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URLDecoder;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class S3Service {
     private final AmazonS3 amazonS3;
 
@@ -32,5 +37,31 @@ public class S3Service {
 
         // 업로드된 파일의 URL 반환
         return amazonS3.getUrl(bucketName, fileName).toString();
+    }
+
+    public void deleteFile(String fileUrl) {
+        try {
+            if (fileUrl != null && ! fileUrl.isEmpty()) {
+                if (fileUrl.contains("amazonaws.com")) {
+                    // URL에서 파일 키 추출
+                    String fileKey = fileUrl.substring(fileUrl.indexOf(".com/") + 5); // ".com/" 이후 경로 추출
+                    fileKey = URLDecoder.decode(fileKey.trim(), "UTF-8"); // 디코딩 및 정리
+//
+//                    log.info("버킷 이름: {}", bucketName);
+//                    log.info("파일 키: {}", fileKey);
+
+                    // S3에서 파일 존재 확인
+                    if (amazonS3.doesObjectExist(bucketName, fileKey)) {
+                        amazonS3.deleteObject(new DeleteObjectRequest(bucketName, fileKey));
+                        log.info("S3 파일 삭제됨: {}", fileKey);
+                    } else {
+                        log.warn("S3에서 파일이 존재하지 않음: {}", fileKey);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error("파일 삭제 중 오류 발생: {}", e.getMessage());
+            throw new S3FileNotDeletedException("파일 삭제 중 오류 발생");
+        }
     }
 }
