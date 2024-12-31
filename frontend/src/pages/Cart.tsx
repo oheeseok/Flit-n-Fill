5ae2d125e859b29751e07754a141d368d2fbe9d3
Gahyeon19 <wjsrkgus0119@naver.com>
2024-12-31 16:12:15 +0900

    feat: proxy 설정 / cart 페이지 오류 해결 (#107)

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -9,9 +9,9 @@ const Cart = () => {
   // 서버에서 장바구니 데이터 조회하는 함수
   const getMyCart = async () => {
     try {
-      const response = await axios.get("/api/my-fridge/shoppingcart", 
+      const response = await axios.get("/api/my-fridge/shoppingcart",
         {
-          headers: { "Content-Type": "application/json"},
+          withCredentials: true,
         }
       );
       // setInputText(response.data.join("\n")); // textarea에 표시할 텍스트로 변환
@@ -20,7 +20,7 @@ const Cart = () => {
       console.error("장바구니 조회 실패:", error);
       alert("장바구니 조회 중 문제가 발생했습니다.");
     }
-  }
+  };
 
   useEffect(() => {
     getMyCart();
@@ -45,8 +45,8 @@ const Cart = () => {
   // };
 
   // 텍스트 입력 처리 함수
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInputText(e.target.value); 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
   };
 
   // 줄 삭제 버튼 클릭
@@ -64,18 +64,18 @@ const Cart = () => {
 
   // 저장 버튼 클릭 시 서버로 데이터 전송
   const handleSave = async () => {
-      const memo = inputText.split("\n").filter((line) => line.trim() !== ""); // 리스트 생성
-      console.log("inputText: ", inputText)
-      console.log("memo: ", memo)
+    const memo = inputText.split("\n").filter((line) => line.trim() !== ""); // 리스트 생성
+    console.log("inputText: ", inputText);
+    console.log("memo: ", memo);
     try {
       const response = await axios.post(
         "/api/my-fridge/shoppingcart", // 백엔드 API 경로
         lines, // 리스트를 전송
         {
-          headers: { "Content-Type": "application/json" }
+          withCredentials: true,
         }
       );
-      console.log("response: ", response)
+      console.log("response: ", response);
       if (response.status === 201) {
         alert("장바구니 저장 성공!");
       }
@@ -83,14 +83,13 @@ const Cart = () => {
       console.error("장바구니 저장 실패:", error);
       alert("장바구니 저장 중 문제가 발생했습니다.");
     }
-  }
+  };
 
   return (
     <div className="cartbody">
       <div className="cart-container">
         <div className="cart-title">cart</div>
         <div className="cart-textbox">
-
         <div className="cart-fake-textarea">
           {lines.map((line, index) => (
             <div key={index} className="cart-line">
@@ -106,20 +105,21 @@ const Cart = () => {
           ))}
         </div>
 
-        {/* 입력창 */}
-        <div className="cart-input-container">
-          <input
-            type="text"
-            className="cart-input"
-            value={inputText}
-            placeholder="사야할 재료를 입력해주세요."
-            onChange={handleInputChange}
-          />
-          <button className="add-button" onClick={handleAddLine}>
-            추가
-          </button>
-        </div>
-
+          {/* 입력창 */}
+          <div className="cart-input-container">
+            <input
+              type="text"
+              className="cart-input"
+              value={inputText}
+              placeholder="사야할 재료를 입력해주세요."
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                handleInputChange(e)
+              }
+            />
+            <button className="add-button" onClick={handleAddLine}>
+              추가
+            </button>
+          </div>
         </div>
         <button className="save-cart-button" onClick={handleSave}>
           저장하기
