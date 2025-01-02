723d64e5b6ddf58dba0f02f4a20359eddb961c04
Gahyeon19 <wjsrkgus0119@naver.com>
2025-01-02 12:28:55 +0900

    fix: Cart 수정 (#107)

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -24,26 +24,8 @@ const Cart = () => {
     getMyCart();
   }, []);
 
-  // 엔터키로 텍스트를 태그로 추가하는 함수
-  // const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
-  //   if (e.key === "Enter" && inputText.trim() !== "") {
-  //     const newTag = inputText.trim();
-  //     setInputText(""); // 입력 후 텍스트 초기화
-
-  //     // 입력된 텍스트를 태그 형식으로 변환
-  //     const tagElement = document.createElement("span");
-  //     tagElement.className = "tag"; // 태그 스타일 적용
-  //     tagElement.textContent = newTag;
-
-  //     // e.preventDefault(); // 엔터키가 줄 바꿈을 하지 않도록 방지
-
-  //     const contentEditable = e.target as HTMLDivElement;
-  //     contentEditable.appendChild(tagElement); // contentEditable 안에 태그 추가
-  //   }
-  // };
-
   // 텍스트 입력 처리 함수
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInputText(e.target.value);
   };
 
@@ -109,7 +91,7 @@ const Cart = () => {
               className="cart-input"
               value={inputText}
               placeholder="사야할 재료를 입력해주세요."
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e)}
+              onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => handleInputChange(e)}
             />
             <button className="add-button" onClick={handleAddLine}>
               추가
