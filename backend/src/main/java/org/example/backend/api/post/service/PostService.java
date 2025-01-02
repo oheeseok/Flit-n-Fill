c30f611ebdb24f1594e1941c89096fa2a93f6def
oheeseok <heeseok96@gmail.com>
2025-01-02 19:12:43 +0900

    feat: 알림뱃지에 저장되는 메시지 형식 수정 (#124)

--- backend/src/main/java/org/example/backend/api/post/service/PostService.java
+++ backend/src/main/java/org/example/backend/api/post/service/PostService.java
@@ -295,7 +295,7 @@ public class PostService {
     notificationService.saveTradeRequestNotification(
         writer,
         notificationType,
-        post.getTradeType().getDescription() + " 요청이 왔습니다!",
+        proposer.getUserNickname() + "님이 " + post.getTradeType().getDescription() + "을 요청합니다.",
         tradeRequest.getTradeRequestId(),
         null);
     log.info("3. save data to db --- done");
