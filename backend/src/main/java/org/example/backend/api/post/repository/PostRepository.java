7db92da2773b92889439cb266afbef83f58edc04
Gahyeon19 <wjsrkgus0119@naver.com>
2025-01-03 15:18:14 +0900

    feat: 게시글 검색 가능 (교환,나늠 모두 검색 됨) (#120)

--- backend/src/main/java/org/example/backend/api/post/repository/PostRepository.java
+++ backend/src/main/java/org/example/backend/api/post/repository/PostRepository.java
@@ -11,10 +11,10 @@ public interface PostRepository extends JpaRepository<Post, Long> {
   List<Post> findAllByOrderByPostCreatedDateDesc();
   @Query("SELECT p FROM Post p " +
       "JOIN p.writerFood f " +
-      "JOIN p.proposerFoodList fl " +
+      "LEFT JOIN p.proposerFoodList fl " +
       "WHERE (LOWER(f.foodListName) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-      "OR LOWER(fl.foodListProduct) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-      "OR LOWER(fl.foodListType) LIKE LOWER(CONCAT('%', :keyword, '%'))) " +
+      "OR (fl IS NOT NULL AND LOWER(fl.foodListProduct) LIKE LOWER(CONCAT('%', :keyword, '%'))) " +
+      "OR (fl IS NOT NULL AND LOWER(fl.foodListType) LIKE LOWER(CONCAT('%', :keyword, '%')))) " +
       "ORDER BY p.postCreatedDate DESC")
   List<Post> findByKeyword(@Param("keyword") String keyword);
 }
