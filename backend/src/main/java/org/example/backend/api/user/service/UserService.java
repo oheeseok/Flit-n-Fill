427e1ccd9cdc6343afc78ef6afa1e2072f54a586
byulhan <hanbyul933@gmail.com>
2025-01-04 19:49:20 +0900

    feat: validation 추가(#178) - #178

--- backend/src/main/java/org/example/backend/api/user/service/UserService.java
+++ backend/src/main/java/org/example/backend/api/user/service/UserService.java
@@ -162,14 +162,13 @@ public class UserService {
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
 
         user.setUserNickname(updateDto.getUserNickname() != null ? updateDto.getUserNickname() : user.getUserNickname());
-        if (updateDto.getUserPassword() != null && !passwordEncoder.matches(updateDto.getUserPassword(), user.getUserPassword())) {
+        if (updateDto.getUserPassword() != null && !updateDto.getUserPassword().equals("") && !passwordEncoder.matches(updateDto.getUserPassword(), user.getUserPassword())) {
             String password = passwordEncoder.encode(updateDto.getUserPassword());
             user.setUserPassword(password);
         }
         user.setUserPhone(updateDto.getUserPhone() != null ? updateDto.getUserPhone() : user.getUserPassword());
         user.setUserAddress(updateDto.getUserAddress() != null ? updateDto.getUserAddress() : user.getUserAddress());
         if (userProfile != null && !userProfile.equals(PROFILE_DEFAULT_IMG_URL)) {
-            s3Service.deleteFile(user.getUserProfile());
             String newProfile = s3Service.uploadFile(userProfile, "users/profile");
             user.setUserProfile(newProfile);
         } else {  // 프로필 삭제 시 기본 프로필로 변경
