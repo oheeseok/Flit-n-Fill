b3e29e4a0564e87744f509e4bea6962d08186365
oheeseok <heeseok96@gmail.com>
2025-01-06 12:28:25 +0900

    feat: 로그아웃 상태에서 메인페이지 접속 시 레시피 사진 보이게 설정 완료 (#202)

--- backend/src/main/java/org/example/backend/security/SpringConfig.java
+++ backend/src/main/java/org/example/backend/security/SpringConfig.java
@@ -22,99 +22,100 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @RequiredArgsConstructor
 @Configuration
 public class SpringConfig {
-    private final JwtTokenProvider jwtTokenProvider;
-    private final TokenManagementService tokenManagementService;
-    private final CustomOAuth2UserService oAuth2UserService;
-    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
-    private final CustomUserDetailsService customUserDetailsService;
+  private final JwtTokenProvider jwtTokenProvider;
+  private final TokenManagementService tokenManagementService;
+  private final CustomOAuth2UserService oAuth2UserService;
+  private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+  private final CustomUserDetailsService customUserDetailsService;
 
-    // password encoder
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  // password encoder
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() { // security를 적용하지 않을 리소스
-        return web -> web.ignoring()
-                // error endpoint를 열어줘야 함, favicon.ico 추가!
-                .requestMatchers("/error", "/favicon.ico");
-    }
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() { // security를 적용하지 않을 리소스
+    return web -> web.ignoring()
+        // error endpoint를 열어줘야 함, favicon.ico 추가!
+        .requestMatchers("/error", "/favicon.ico");
+  }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // 인증 요청 페이지 설정
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/user/register", "/api/user/login", "api/user/login/social", "/oauth2/**", "/api/subscribe/**", "/api/auth/**",
-                                "/api/admin/login", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/**").authenticated()
-                        .anyRequest().permitAll())
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // 인증 요청 페이지 설정
+    http
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/api/user/register", "/api/user/login", "api/user/login/social", "/oauth2/**", "/api/subscribe/**", "/api/auth/**",
+                "/api/admin/login", "/swagger-ui/**", "/v3/api-docs/**", "/api/user/recipes", "/api/user/recipes/").permitAll()
+            .requestMatchers("/api/user/recipes/{recipeId}")
+            .permitAll() // GET /api/user/recipes/{recipeId} 허용
+            .requestMatchers("/api/admin/**").hasRole("ADMIN")
+            .requestMatchers("/api/**").authenticated()
+            .anyRequest().permitAll())
 
-                // REST API 이기 때문에 basic auth 및 csrf 보안 미사용
-                .httpBasic(basic -> basic.disable())
-                .csrf(csrf -> csrf.disable())
+        // REST API 이기 때문에 basic auth 및 csrf 보안 미사용
+        .httpBasic(basic -> basic.disable())
+        .csrf(csrf -> csrf.disable())
 
-                // jwt 토큰을 사용하기 때문에 session 사용하지 않고 stateless 서버 생성
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        // jwt 토큰을 사용하기 때문에 session 사용하지 않고 stateless 서버 생성
+        .sessionManagement(session ->
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-                // form 로그인 방식 비활성화
-                .formLogin(login -> login.disable())
+        // form 로그인 방식 비활성화
+        .formLogin(login -> login.disable())
 
-                // OAuth2 로그인 추가
-                .oauth2Login(oauth2 ->
-                        oauth2.userInfoEndpoint(c -> c.userService(oAuth2UserService))
-                                .successHandler(oAuth2LoginSuccessHandler))// 로그인 성공시 이동할 URL
+        // OAuth2 로그인 추가
+        .oauth2Login(oauth2 ->
+            oauth2.userInfoEndpoint(c -> c.userService(oAuth2UserService))
+                .successHandler(oAuth2LoginSuccessHandler))// 로그인 성공시 이동할 URL
 
         // jwt token filter
-                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
         // cors 설정
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
 
+    return http.build();
+  }
 
-        return http.build();
-    }
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.addAllowedOrigin("http://localhost:5173"); // React 개발 서버 주소
+    configuration.addAllowedOrigin("http://localhost:4173"); // React 개발 서버 주소
+    configuration.addAllowedOrigin("http://54.180.201.220/");
+    configuration.addAllowedOrigin("http://54.180.201.220"); // React 개발 서버 주소
+    configuration.addAllowedOrigin("http://3.39.21.24"); // React 개발 서버 주소
+    configuration.addAllowedOrigin("http://52-1068271922.ap-northeast-2.elb.amazonaws.com/"); // React 개발 서버 주소
+    configuration.addAllowedOrigin("https://52-1068271922.ap-northeast-2.elb.amazonaws.com/"); // React 개발 서버 주소
+    configuration.addAllowedOrigin("http://www.flitnfill.kro.kr/"); // React 개발 서버 주소
+    configuration.addAllowedOrigin("http://www.flitnfill.kro.kr:8080/"); // React 개발 서버 주소
+    configuration.addAllowedOrigin("https://www.flitnfill.kro.kr/"); // React 개발 서버 주소
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:5173"); // React 개발 서버 주소
-        configuration.addAllowedOrigin("http://localhost:4173"); // React 개발 서버 주소
-        configuration.addAllowedOrigin("http://54.180.201.220/");
-        configuration.addAllowedOrigin("http://54.180.201.220"); // React 개발 서버 주소
-        configuration.addAllowedOrigin("http://3.39.21.24"); // React 개발 서버 주소
-        configuration.addAllowedOrigin("http://52-1068271922.ap-northeast-2.elb.amazonaws.com/"); // React 개발 서버 주소
-        configuration.addAllowedOrigin("https://52-1068271922.ap-northeast-2.elb.amazonaws.com/"); // React 개발 서버 주소
-        configuration.addAllowedOrigin("http://www.flitnfill.kro.kr/"); // React 개발 서버 주소
-        configuration.addAllowedOrigin("http://www.flitnfill.kro.kr:8080/"); // React 개발 서버 주소
-        configuration.addAllowedOrigin("https://www.flitnfill.kro.kr/"); // React 개발 서버 주소
+    configuration.addAllowedMethod("*");
+    configuration.addAllowedHeader("*");
+    configuration.setAllowCredentials(true);
 
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+  @Bean
+  public JwtAuthenticationFilter jwtAuthenticationFilter() {
+    return new JwtAuthenticationFilter(jwtTokenProvider, tokenManagementService, customUserDetailsService); // JwtTokenProvider 주입
+  }
 
-    @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter(jwtTokenProvider, tokenManagementService, customUserDetailsService); // JwtTokenProvider 주입
-    }
+  @Bean
+  public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilterRegistration(JwtAuthenticationFilter jwtAuthenticationFilter) {
+    FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+    registrationBean.setFilter(jwtAuthenticationFilter); // 필터에 JwtAuthenticationFilter 주입
+    return registrationBean;
+  }
 
-    @Bean
-    public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilterRegistration(JwtAuthenticationFilter jwtAuthenticationFilter) {
-        FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(jwtAuthenticationFilter); // 필터에 JwtAuthenticationFilter 주입
-        return registrationBean;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
 }
